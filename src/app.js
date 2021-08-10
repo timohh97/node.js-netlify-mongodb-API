f0e70abfff616161d719e4a921be4fdeccfdb0e2
Timo <timo.schessl@gmx.de>
2021-08-10 18:30:44 +0200

    New version

--- src/app.js
+++ src/app.js
@@ -4,16 +4,21 @@ const mongoose = require("mongoose");
 const routes = require("./routes/get");
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const serverless = require("serverless-http")
 
 app.use(bodyParser.json())
-app.use("/",routes)
+app.use("/.netlify/functions/app",routes)
 app.use(cors())
-app.use(express.static("Frontend"))
+app.use(express.static("dist"))
 
 mongoose.connect("mongodb+srv://mainuser:test@cluster.0gtou.mongodb.net/rest?retryWrites=true&w=majority", 
 { useNewUrlParser: true, useUnifiedTopology: true },() =>
 {
     console.log("Connected to database.")
 })
+
+module.exports.handler = serverless(app);
+
+
 
 app.listen(3000);
